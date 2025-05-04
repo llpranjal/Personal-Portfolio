d27c268f59e37e0be00a54a51b3647a082c7b5c9
pranjal <kattelpranjal01@gmail.com>
2025-05-03 22:31:41 -0400

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,14 +3,13 @@ import Projects from "./components/sections/Projects";
 import Footer from "./components/Footer";
 import Stacks from "./components/sections/Stacks";
 import ContactMe from "./components/sections/ContactMe";
-import Me from "./components/sections/Me";
+
 
 export default function Layout() {
   return (
     <>
       <div className="px-6 md:px-12 lg:px-20">
         <Name />
-        <Me />
         <Projects />
         <Stacks />
         <ContactMe />
